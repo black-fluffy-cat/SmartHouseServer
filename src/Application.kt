e5576a1b6d80fbbcf20a24f6eeb48b1cb4702f03
jj <>
2020-11-23 14:35:24 +0100

    Configure alert call to Raspberry

--- src/Application.kt
+++ src/Application.kt
@@ -1,16 +1,16 @@
 package com.jj.smarthouseserver
 
-import io.ktor.application.call
-import io.ktor.application.install
-import io.ktor.features.ContentNegotiation
-import io.ktor.http.ContentType
-import io.ktor.jackson.JacksonConverter
-import io.ktor.request.receive
-import io.ktor.response.respond
-import io.ktor.routing.post
-import io.ktor.routing.routing
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
+import io.ktor.application.*
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.jackson.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.slf4j.Logger
@@ -21,7 +21,14 @@ private val logger: Logger = LoggerFactory.getLogger("MainLogger")
 data class HeartbeatData(val deviceName: String? = null, val timeFromStart: String, val timeFromAlert: String)
 data class AlertData(val deviceName: String? = null, val timeFromStart: String, val alertState: String)
 
+private lateinit var httpClient: HttpClient
+private lateinit var raspberryCallManager: RaspberryCallManager
+
 fun main(args: Array<String>) {
+
+    httpClient = HttpClient(CIO)
+    raspberryCallManager = RaspberryCallManager(httpClient, logger)
+
     val server = embeddedServer(Netty, 8080) {
         install(ContentNegotiation) {
             register(ContentType.Application.Json, JacksonConverter())
@@ -35,6 +42,8 @@ fun main(args: Array<String>) {
                                 " alertState: ${alertData.alertState}"
                     )
                     call.respond(mapOf("OK" to true))
+
+                    raspberryCallManager.pingRaspberryToMakePhoto()
                 }
             }
             post("/heartbeat") {
