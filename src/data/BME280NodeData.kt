1748935f41e6668f972a3add059837d95bb55abb
jj <>
2021-01-25 08:09:59 +0100

    Add todo

--- src/data/BME280NodeData.kt
+++ src/data/BME280NodeData.kt
@@ -1,5 +1,6 @@
 package com.jj.smarthouseserver.data
 
+//TODO Change to BMP - used sensor does not support reading humidity and is BMP280
 data class BME280NodeData(
     val deviceName: String,
     val temperatureC: String,
